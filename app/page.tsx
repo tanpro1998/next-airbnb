ac164581bc473bdb0c046ff6f277a021612b0086
tanpro1998 <davidtanpro1998@gmail.com>
2023-04-17 17:33:23 +0700

    commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -25,15 +25,13 @@ const Home = async ({ searchParams }: HomeProps) => {
     <ClientOnly>
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                key={listing.id}
-                data={listing}
-                currentUser={currentUser}
-              />
-            );
-          })}
+          {listings.map((listing: any) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
